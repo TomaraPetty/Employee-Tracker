57469044986ca02b78fe1bc1432f39badff1c0e1
Tomara Petty <tomararuth@gmail.com>
2021-02-08 07:06:58 -0800

    adding three tables to employee database.

--- employees.sql
+++ employees.sql
@@ -1,2 +1,31 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE database employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE emp_role (
+  role_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(7,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (role_id)
+);
+
+CREATE TABLE department (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  dep_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+
+SELECT * FROM employee;
+SELECT * FROM emp_role;
+SELECT * FROM department
